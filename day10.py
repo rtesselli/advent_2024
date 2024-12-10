956b377a82bcbb5c95008bee2e910da75926c842
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2024-12-10 10:45:39 +0100

    Add day10 part 2

--- day10.py
+++ day10.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pathlib import Path
+from collections import Counter
 
 Point = tuple[int, int]
 
@@ -13,23 +14,26 @@ def in_bounds(x, y):
     return 0 <= x < tmap.shape[0] and 0 <= y < tmap.shape[1]
 
 
-def count_trailheads(matrix: np.ndarray) -> int:
-    def rec(row: int, col: int) -> set[Point]:
+def count_trailheads(matrix: np.ndarray) -> tuple[int, int]:
+    def rec(row: int, col: int, counter: Counter) -> Counter:
         if matrix[row, col] == 9:
-            return {(row, col)}
+            counter[(row, col)] += 1
+            return counter
         neighbors = [(row + delta_row, col + delta_col) for delta_row, delta_col in ((-1, 0), (1, 0), (0, -1), (0, 1))
                      if in_bounds(row + delta_row, col + delta_col)]
-        destinations = set()
         for next_row, next_col in neighbors:
             if matrix[next_row, next_col] == matrix[row, col] + 1:
-                destinations = destinations.union(rec(next_row, next_col))
-        return destinations
+                rec(next_row, next_col, counter)
+        return counter
 
     starts = np.where(matrix == 0)
     counts = 0
+    trails = 0
     for row, col in zip(*starts):
-        counts += len(rec(row, col))
-    return counts
+        result = rec(row, col, Counter())
+        counts += len(result)
+        trails += sum(n for n in result.values())
+    return counts, trails
 
 
 if __name__ == '__main__':
